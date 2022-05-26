c3da666840b23882c3a20ec9554e4a8a7dcb7332
Tricster <mediosrity@qq.com>
2022-05-26 10:46:14 +0800

    Move header content into file.

---
+++ source/loaders/rs_loader/rust/compiler/src/wrapper/header.rs
@@ -0,0 +1,30 @@
+use std::{
+    ffi::{c_void, CStr, CString},
+    os::raw::{c_char, c_double, c_float, c_int, c_long, c_short},
+};
+extern "C" {
+    fn value_type_count(v: *mut c_void) -> c_int;
+    fn metacall_value_id(v: *mut c_void) -> c_int;
+    fn metacall_value_to_int(v: *mut c_void) -> c_int;
+    fn metacall_value_to_bool(v: *mut c_void) -> c_int;
+    fn metacall_value_to_char(v: *mut c_void) -> c_char;
+    fn metacall_value_to_long(v: *mut c_void) -> c_long;
+    fn metacall_value_to_short(v: *mut c_void) -> c_short;
+    fn metacall_value_to_float(v: *mut c_void) -> c_float;
+    fn metacall_value_to_double(v: *mut c_void) -> c_double;
+    fn metacall_value_to_array(v: *mut c_void) -> *mut *mut c_void;
+    fn metacall_value_to_map(v: *mut c_void) -> *mut *mut c_void;
+    fn metacall_value_to_ptr(v: *mut c_void) -> *mut c_void;
+    fn metacall_value_to_string(v: *mut c_void) -> *mut c_char;
+    fn metacall_function(cfn: *const c_char) -> *mut c_void;
+    fn metacall_value_create_int(i: c_int) -> *mut c_void;
+    fn metacall_value_create_bool(b: c_int) -> *mut c_void;
+    fn metacall_value_create_long(l: c_long) -> *mut c_void;
+    fn metacall_value_create_char(st: c_char) -> *mut c_void;
+    fn metacall_value_create_short(s: c_short) -> *mut c_void;
+    fn metacall_value_create_float(f: c_float) -> *mut c_void;
+    fn metacall_value_create_double(d: c_double) -> *mut c_void;
+    fn metacall_value_create_string(st: *const c_char, ln: usize) -> *mut c_void;
+    fn metacall_value_create_array(values: *const *mut c_void, size: usize) -> *mut c_void;
+    fn metacall_value_create_map(tuples: *const *mut c_void, size: usize) -> *mut c_void;
+}
--- source/loaders/rs_loader/rust/compiler/src/wrapper/mod.rs
+++ source/loaders/rs_loader/rust/compiler/src/wrapper/mod.rs
@@ -11,7 +11,17 @@ use super::{
 };
 use std::fmt;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
+use std::path::PathBuf;
+
+#[cfg(test)]
+lazy_static::lazy_static! {
+    static ref WRAPPER_DIR: &'static str = "DUMMY";
+}
+#[cfg(not(test))]
+lazy_static::lazy_static! {
+    static ref WRAPPER_DIR: &'static str = env!("WRAPPER_DIR");
+}
 
 trait Wrapper {
     fn as_arg(&self) -> String;
@@ -202,43 +212,21 @@ pub unsafe fn metacall_{}(args_p: *mut *mut c_void, size: usize) -> *mut c_void 
     }
 }
 
+fn read_file(file_name: &str) -> std::io::Result<String> {
+    let mut ret = String::new();
+    let mut file_path = PathBuf::from(WRAPPER_DIR.to_string());
+    file_path.push(file_name);
+    let mut file = std::fs::File::open(file_path)?;
+    file.read_to_string(&mut ret)?;
+    Ok(ret)
+}
+
 pub fn generate_wrapper(callbacks: CompilerCallbacks) -> std::io::Result<CompilerCallbacks> {
     let mut wrapped_functions: Vec<Function> = vec![];
     let mut content = String::new();
-    content.push_str(
-        "
-use std::{
-    ffi::{c_void, CString, CStr},
-    os::raw::{c_char, c_double, c_float, c_int, c_long, c_short},
-};
-extern \"C\" {
-    fn value_type_count(v: *mut c_void) -> c_int;
-    fn metacall_value_id(v: *mut c_void) -> c_int;
-    fn metacall_value_to_int(v: *mut c_void) -> c_int;
-    fn metacall_value_to_bool(v: *mut c_void) -> c_int;
-    fn metacall_value_to_char(v: *mut c_void) -> c_char;
-    fn metacall_value_to_long(v: *mut c_void) -> c_long;
-    fn metacall_value_to_short(v: *mut c_void) -> c_short;
-    fn metacall_value_to_float(v: *mut c_void) -> c_float;
-    fn metacall_value_to_double(v: *mut c_void) -> c_double;
-    fn metacall_value_to_array(v: *mut c_void) -> *mut *mut c_void;
-    fn metacall_value_to_map(v: *mut c_void) -> *mut *mut c_void;
-    fn metacall_value_to_ptr(v: *mut c_void) -> *mut c_void;
-    fn metacall_value_to_string(v: *mut c_void) -> *mut c_char;
-    fn metacall_function(cfn: *const c_char) -> *mut c_void;
-    fn metacall_value_create_int(i: c_int) -> *mut c_void;
-    fn metacall_value_create_bool(b: c_int) -> *mut c_void;
-    fn metacall_value_create_long(l: c_long) -> *mut c_void;
-    fn metacall_value_create_char(st: c_char) -> *mut c_void;
-    fn metacall_value_create_short(s: c_short) -> *mut c_void;
-    fn metacall_value_create_float(f: c_float) -> *mut c_void;
-    fn metacall_value_create_double(d: c_double) -> *mut c_void;
-    fn metacall_value_create_string(st: *const c_char, ln: usize) -> *mut c_void;
-    fn metacall_value_create_array(values: *const *mut c_void, size: usize) -> *mut c_void;
-    fn metacall_value_create_map(tuples: *const *mut c_void, size: usize) -> *mut c_void;
-}
-    ",
-    );
+
+    content.push_str(read_file("header.rs")?.as_str());
+
     for func in callbacks.functions.iter() {
         let wrapper_func = WrapperFunction::new(func);
         let wrapper = wrapper_func.generate();
